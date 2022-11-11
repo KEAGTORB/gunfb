27c7c77961d5e6b6acfe78dcb3740dc768c5c0d1
KIYAG <92469140+KEAGTORB@users.noreply.github.com>
2022-11-11 18:03:21 +0530

    Add files via upload

---
+++ gufb.py
@@ -0,0 +1,26 @@
+from github import Github
+from github.GithubException import GithubException
+from getpass import getpass
+
+if __name__ == '__main__':
+    gitHubPAT = getpass('enter your Github personal access token: ').strip()
+    
+    gitUser = Github(gitHubPAT)
+
+    try:
+        print('Your Github ID is: {}\nYour Github Name is: {}'.format(gitUser.get_user().id, gitUser.get_user().name))
+        print('You got {} followers and following {} Github users...'.format(gitUser.get_user().followers, gitUser.get_user().following))
+    except GithubException as e:
+        print('This script can\'t connect to Github with following information:\n\Token: {}'.format(gitHubPAT))
+        print('Run this script again if you want to use it... :)')
+        exit()
+
+    followers = [follower.id for follower in gitUser.get_user().get_followers()] 
+
+    print('\n\n')
+
+    for following in gitUser.get_user().get_following():
+        print('Checking Github ID: {} and Github Name: {}'.format(following.id, following.name))
+        if following.id not in followers:
+            gitUser.get_user().remove_from_following(following)
+            print('Github user with ID: {} and Name: {} is not following you and automaticaly unfollowed by this script'.format(following.id, following.name))
